2a169db5c426d3b42a368a5292d307f612095c12
Mario Souza <mario.mhsouza@gmail.com>
2022-04-12 17:04:34 -0300

    Obtendo um post por id

---
+++ src/main/java/com/mariomhs/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.mariomhs.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mariomhs.workshopmongo.domain.Post;
+import com.mariomhs.workshopmongo.repository.PostRepository;
+import com.mariomhs.workshopmongo.resources.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
